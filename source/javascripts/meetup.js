c43680c0ea9a7df7003e53b3a641a7dfc59a3d3d
Mike Ball <mikedball@gmail.com>
2013-11-16 14:18:37 -0500

    Adds sig & sig_id params to API call

--- source/javascripts/meetup.js
+++ source/javascripts/meetup.js
@@ -12,7 +12,7 @@ define('meetup', [
   };
 
   Meetup.prototype.events = function (opts) {
-    var path = '/events.json?radius=25.0&order=time&group_urlname=phillyrb&offset=0&format=json&page=200&fields=&sig_id=REPLACE_ME&sig=REPLACE_ME';
+    var path = '/events.json?radius=25.0&order=time&group_urlname=phillyrb&offset=0&format=json&page=200&fields=&sig_id=85981882&sig=8e2446bfa10cfee4340f1441481163d1c7b255ff';
 
     this._fetchData(path, opts);
   };
